dd4ae0121d05c0fd3ebc251aefd42ea775525a04
Ionut Daniel <ionut.daniel644@gmail.com>
2023-01-19 15:30:00 +0200

    work

--- azonix-client/src/common/PageProvider/PanelProvider/PanelProvider.js
+++ azonix-client/src/common/PageProvider/PanelProvider/PanelProvider.js
@@ -1,112 +1,158 @@
-import React, { useRef } from "react";
+/* eslint-disable no-restricted-globals */
+/* eslint-disable react/no-direct-mutation-state */
+import React, { createRef } from "react";
 import _ from "lodash";
-import { Responsive, WidthProvider } from "react-grid-layout"
-import { SCREEN_MAPPER } from './screenMapper'
+import { Responsive, WidthProvider } from "react-grid-layout";
+import { SCREEN_MAPPER } from "./screenMapper";
+import ConsolePanel from "../../../panels/Console/ConsolePanel";
+import { When } from "react-if";
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 export default class PanelProvider extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            items: [],
-        };
-        this.onBreakpointChange = this.onBreakpointChange.bind(this);
-    }
-
-    static defaultProps = {
-        onLayoutChange: function () { },
-    };
-
     state = {
         currentBreakpoint: "lg",
         mounted: true,
+        layout: [],
+        cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
     };
 
-    componentDidMount() {
-        this.setState({ mounted: true });
-    }
-
-    onBreakpointChange(breakpoint, cols) {
+    onBreakpointChange = (breakpoint) => {
+        console.log(breakpoint);
+        console.log(this.state.layout);
         this.setState({
             currentBreakpoint: breakpoint,
-            cols: cols
         });
-    }
+    };
 
     onRemoveItem(i) {
-        this.setState({ items: _.reject(this.state.items, { i: i }) });
+        this.setState({ layout: _.reject(this.state.layout, { i: i }) });
     }
 
     onDrop = (layout, layoutItem, _event) => {
-        const cardData = _event.dataTransfer.getData('text/plain')
-        const screen = SCREEN_MAPPER[cardData]
+        console.log(layoutItem);
+        const availableHandles = ["s", "w", "e", "n"];
+        const cardData = _event.dataTransfer.getData("text/plain");
+        const screen = SCREEN_MAPPER[cardData];
+        const { x, y, w, h } = this.calculateLogic();
         this.setState({
-            items: this.state.items.concat({
+            layout: this.state.layout.concat({
                 i: screen.id + Math.random(),
-                x: layoutItem.x,
-                y: layoutItem.y,
-                w: 6,
-                h: 2,
+                x: x,
+                y: y,
+                w: w,
+                h: h,
                 panelName: screen.panelName,
                 panelComponent: screen.panelComponent,
-                static: true,
+                static: false,
+                resizeHandles: availableHandles,
             }),
         });
+        this.renderDom();
+    };
+
+    calculateLogic = () => {
+        var x = 0;
+        var y = 0;
+        var w = 4;
+        var h = 2.8;
+        switch (this.state.currentBreakpoint) {
+            case "lg":
+                const getCurrentBreakpoint =
+                    this.state.cols[this.state.currentBreakpoint];
+                if (this.state.layout.length === 6) {
+                    x = 0;
+                    y = 0;
+                    w = 4;
+                    h = 3;
+                }
+                break;
+            default:
+                break;
+        }
+
+        return { x, y, w, h };
     };
 
     createElement(el) {
-        const removeStyle = {
-            position: "absolute",
-            right: "2px",
-            top: 0,
-            cursor: "pointer"
-        };
         return (
-            <div key={el.i} data-grid={el}>
-                <span className="text">
-                    <span className="react-grid-dragHandleExample" onMouseEnter={() => {
-                        el.static = false
-                        console.log(el.static)
-                    }}>[DRAG HERE]</span>
-                </span>
-                <span
-                    className="remove"
-                    style={removeStyle}
-                    onClick={this.onRemoveItem.bind(this, el.i)}
+            <div key={el.i} data-grid={el} className={"z-[60]"}>
+                <div
+                    style={{
+                        height: 28,
+                        width: "100%",
+                        background: "#000",
+                    }}
                 >
-                    x
-                </span>
-                {el.panelComponent}
-
+                    <span className="text">
+                        <span className="react-grid-dragHandleExample text-white">
+                            [DRAG HERE]
+                        </span>
+                    </span>
+                    <span
+                        className="remove text-white"
+                        style={{
+                            position: "absolute",
+                            right: "2px",
+                            top: 0,
+                            cursor: "pointer",
+                        }}
+                        onClick={this.onRemoveItem.bind(this, el.i)}
+                    >
+                        x
+                    </span>
+                </div>
+                <div style={{ height: "calc(100% - 28px)" }}>
+                    {el.panelComponent}
+                </div>
             </div>
         );
     }
 
+    renderDom() {
+        console.log(this.state.layout);
+        return _.map(this.state.layout, (el) => this.createElement(el));
+    }
 
     render() {
         return (
-            <div
-                className="w-auto h-full ml-20 overflow-x-hidden overflow-y-auto bg-red-400"
-            >
-                <ResponsiveReactGridLayout
-                    {...this.props}
-                    rowHeight={200}
-                    width={200}
-                    layouts={{}}
-                    onBreakpointChange={this.onBreakpointChange}
-                    onDrop={this.onDrop}
-                    measureBeforeMount={false}
-                    useCSSTransforms={this.state.mounted}
-                    preventCollision={!this.state.compactType}
-                    isDroppable={true}
-                    compactType={'horizontal'}
-
-                >
-                    {_.map(this.state.items, el => this.createElement(el))}
-                </ResponsiveReactGridLayout>
-            </div >
-
-        )
+            <div className="h-full w-full overflow-hidden flex justify-between">
+                <div className="w-3/4 h-full ml-20 overflow-x-hidden overflow-y-auto">
+                    <ResponsiveReactGridLayout
+                        className="z-50"
+                        {...this.props}
+                        layouts={this.state.layout}
+                        autoSize={true}
+                        onDrop={this.onDrop}
+                        measureBeforeMount={false}
+                        useCSSTransforms={this.state.mounted}
+                        preventCollision={!this.state.compactType}
+                        onBreakpointChange={this.onBreakpointChange}
+                        isDroppable={true}
+                        compactType={"horizontal"}
+                        onLayoutChange={this.onLayoutChange}
+                        containerPadding={
+                            this.state.layout.length === 0 ? [30, 30] : [0, 0]
+                        }
+                        margin={
+                            this.state.layout.length === 0 ? [30, 30] : [0, 0]
+                        }
+                    >
+                        {this.renderDom()}
+                    </ResponsiveReactGridLayout>
+                    <div className="panelProvider -z-50">
+                        {_.map(Array(20), (i, index) => (
+                            <span
+                                key={index}
+                                className={`meteor-${index + 1}`}
+                            ></span>
+                        ))}
+                    </div>
+                </div>
+                <div className=" w-1/4 border-l-4 border-slate-800">
+                    <ConsolePanel />
+                </div>
+            </div>
+        );
     }
 }
